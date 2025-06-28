eea22720264cdbd91daabdbeeb16a0fa74786d35
DABONYOURDABIAMCOOL <96404084+DABONYOURDABIAMCOOL@users.noreply.github.com>
2025-06-28 22:11:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, redirect, session, url_for, jsonify
+from flask import Flask, render_template, request, redirect, session, jsonify
 import json, os, datetime
-from collections import defaultdict
 
 app = Flask(__name__)
-app.secret_key = 'XP_TRACKER_SECRET'
+app.secret_key = 'super_secret_key'
 DATA_FILE = 'data/users.json'
 
 def load_users():
@@ -16,110 +15,71 @@ def save_users(users):
     with open(DATA_FILE, 'w') as f:
         json.dump(users, f, indent=4)
 
-def get_class(level):
+def get_title(level):
     if level < 10:
         return "Rookie"
     elif level < 20:
-        return "Novice"
+        return "Beginner"
     elif level < 30:
-        return "Apprentice"
+        return "Fighter"
     elif level < 40:
         return "Elite"
     elif level < 50:
-        return "Master"
-    return "Legend"
+        return "Specialist"
+    else:
+        return "Legend"
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('login.html')
-
-@app.route('/login', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
-    users = load_users()
-    if username in users and users[username]['password'] == password:
-        session['username'] = username
-        return redirect(url_for('dashboard'))
-    return "Invalid login"
+    if request.method == 'POST':
+        users = load_users()
+        uname = request.form['username']
+        pwd = request.form['password']
+        if uname in users and users[uname]['password'] == pwd:
+            session['username'] = uname
+            return redirect('/dashboard')
+        else:
+            return render_template('login.html', error="Invalid credentials")
+    return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
     if 'username' not in session:
-        return redirect(url_for('index'))
+        return redirect('/')
     users = load_users()
-    username = session['username']
-    user_data = users[username]
-    today = datetime.date.today().isoformat()
-
-    # Create leaderboard
+    uname = session['username']
+    user_data = users[uname]
+    level = user_data.get('level', 0)
+    xp = user_data.get('xp', 0)
+    class_title = get_title(level)
+    date_today = datetime.datetime.now().strftime('%d %b %Y')
     leaderboard = sorted([
         {
-            "username": uname,
-            "level": u['level'],
-            "xp": u['xp'],
-            "class": get_class(u['level']),
-            "total": sum(u['stats'].values())
-        } for uname, u in users.items()
+            'name': u,
+            'level': users[u].get('level', 0)
+        } for u in users
     ], key=lambda x: x['level'], reverse=True)
-
-    return render_template('status.html',
-        username=username,
+    return render_template(
+        'status.html',
+        username=uname,
         user_data=user_data,
-        stats=user_data['stats'],
-        level=user_data['level'],
-        xp=user_data['xp'],
-        xp_today=user_data['xp_log'].get(today, 0),
-        xp_needed=100 * user_data['level'],
-        class_name=get_class(user_data['level']),
+        xp=xp,
+        level=level,
+        class_title=class_title,
+        date_today=date_today,
         leaderboard=leaderboard
     )
 
-@app.route('/update_xp', methods=['POST'])
-def update_xp():
-    if 'username' not in session:
-        return '', 403
-    username = session['username']
+@app.route('/update', methods=['POST'])
+def update():
     users = load_users()
-    xp_gain = int(request.form['xp'])
-    user = users[username]
-    user['xp'] += xp_gain
-
-    # Log daily XP
-    today = datetime.date.today().isoformat()
-    user['xp_log'][today] = user['xp_log'].get(today, 0) + xp_gain
-
-    while user['xp'] >= 100 * user['level']:
-        user['xp'] -= 100 * user['level']
-        user['level'] += 1
-
+    uname = session['username']
+    data = request.json
+    users[uname].update(data)
     save_users(users)
-    return jsonify({
-        "xp": user['xp'],
-        "level": user['level'],
-        "xp_needed": 100 * user['level'],
-        "class": get_class(user['level'])
-    })
-
-@app.route('/add_stat', methods=['POST'])
-def add_stat():
-    if 'username' not in session:
-        return '', 403
-    stat = request.form['stat']
-    username = session['username']
-    users = load_users()
-    users[username]['stats'][stat] += 1
-    save_users(users)
-    return '', 200
-
-@app.route('/add_task', methods=['POST'])
-def add_task():
-    return '', 200  # Handled in JS
+    return jsonify(success=True)
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return redirect('/')
